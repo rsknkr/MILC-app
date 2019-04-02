22656e80b04773dbb2b829ce3d8e8acfe5d04a5b
Kira Raskina <kira_raskina@brown.edu>
2019-04-02 12:52:12 -0400

    Using sapply

--- natural-history-simulation.R
+++ natural-history-simulation.R
@@ -2,36 +2,24 @@ library(MILC)
 library(survival)
 
 # vector of follow-up times
-futime <- c()
-# vector of follow-up statuses
-fustat <- c()
-status <- rep(FALSE,100)
+futime <- rep(50,100)
 
-# for each year in time we want to predict
-for (year in 50:120){
-  # make 100 simulations of natural history
-  set.seed(Sys.time())
-  for (i in 1:100){
-    p <- nat_hist ( c(runif(5),50,30), 
-                    120-year, "male", "current", 20, NA, 0.00042, 
-                    c(3.91, 3.91), c(1.1, 1.1), c(2.8, 2.8))
+
+# vector of possible times of death from LC
+dl <- replicate(100, unlist(nat_hist ( c(runif(5),50,30),
+                         20, "male", "current", 20, NA, 0.00042, 
+                         c(3.91, 3.91), c(1.1, 1.1), c(2.8, 2.8))["T_dl"]))
+
+
+# compare t_final with time of lc death   
+status <- sapply(dl, function(x) 70 > x)
     
-    # does survival time exceed t 
-    status[i] <- year > unlist(p["T_dl"]) # TRUE/FALSE  
-    
-  }
 
-futime <- c(futime, rep(year,100))
-fustat <- c(fustat, status)
-  
-}
-
-
-history_simulation_data <- data.frame(Futime = as.numeric(futime), Fustat = fustat)
+history_simulation_data <- data.frame(Futime = as.numeric(futime), Fustat = status)
 
 # create survival object
 surv_object <- Surv(time = history_simulation_data$Futime, 
-                    event =history_simulation_data$Fustat)
+                    event = history_simulation_data$Fustat)
 #surv_object
 
 # Kaplan-Meier estimator
